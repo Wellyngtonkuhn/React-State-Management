d7c05775a6cdd6be8d270fe4a2f8794e3268bda0
Wellyngtonkuhn <kuhnwellyngton@gmail.com>
2025-03-17 16:29:26 -0300

    feature/CU-86a7acbmb:feat - Adding tanstack config

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 export default function App() {
 	return (
 		<BrowserRouter>
 			<Header />
-			<AppRoutes />
+			<QueryClientProvider client={queryClient}>
+				<AppRoutes />
+			</QueryClientProvider>
 			<Footer />
 		</BrowserRouter>
 	);
